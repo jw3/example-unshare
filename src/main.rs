5298be382ae4b360da8fa350e87bd1e4244efdfc
John Wass <jwass3@gmail.com>
2025-01-21 15:42:34 -0500

    subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,126 @@
 use anyhow::{anyhow, Result};
-use clap::{Parser};
-use nix::libc;
-use nix::sched::unshare;
-use nix::sched::CloneFlags;
+use clap::{Parser, Subcommand};
 use nix::unistd::Pid;
-use posix_mq::{Name, Queue};
-use std::ffi::CString;
 use std::process::{Command, Stdio};
+use nix::sched::CloneFlags;
+use posix_mq::{Message, Name, Queue};
 
 #[derive(Parser)]
 struct Opts {
-    #[clap(short, long)]
-    unshare: bool,
+    #[clap(long, default_value = "example")]
+    image_name: String,
 
-    #[clap(short, long, default_value = "/foo")]
-    name: String,
+    #[clap(subcommand)]
+    command: Cmd,
+}
+
+#[derive(Subcommand, Debug)]
+enum Cmd {
+    Rm {
+        names: Vec<String>,
+    },
+    Ls,
+    Mk {
+        #[clap(short, long)]
+        unshare: bool,
+
+        #[clap(short, long)]
+        delete: bool,
+
+        #[clap(short, long)]
+        wait: bool,
+
+        name: String,
+    },
+    Tx {
+        #[clap(short, long, default_value = "/foo")]
+        name: String,
+        message: String,
+    },
 }
 
 fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
 
-    let pid: Pid = nix::unistd::getpid();
-    if opts.unshare {
-        unshare(CloneFlags::CLONE_NEWIPC)?;
-        println!("unshare success: {pid}");
-        println!("namespace: {}", find_namespace(pid)?);
-    } else {
-        let cstr = CString::new(opts.name.clone())?;
-        unsafe {
-            libc::mq_unlink(cstr.as_ptr());
-        };
+    match opts.command {
+        Cmd::Ls => {
+            let mqs = list_mqs()?;
+            if mqs.is_empty() {
+                println!("No MQs found");
+            } else {
+                println!("{}", mqs.join("\n"));
+            }
+        },
+        Cmd::Rm { names } => {
+            for name in names {
+                let qname = Name::new(&name)?;
+                let q = Queue::open(qname).expect("open");
+                q.delete()?;
+                println!("deleted {name}");
+            }
+        }
+        Cmd::Mk { name, wait, delete, unshare } => {
+            let pid: Pid = nix::unistd::getpid();
+            if unshare {
+                println!("unsharing ipc for pid {pid}");
+                nix::sched::unshare(CloneFlags::CLONE_NEWIPC)?;
+                println!("unshare {pid}, lsns: {}", find_namespace(pid)?);
+            }
+            let qname = Name::new(&name)?;
+            let q = Queue::create(qname, 1, 128).expect("create");
+            println!("queue {name} created");
+            if wait {
+                println!(" {}", make_podman_cmd(pid, &name, &opts.image_name));
+                println!("waiting on message..");
+                let result = q.receive()?;
+                println!("received: {:?}", String::from_utf8(result.data)?);
+            }
+
+            if delete {
+                q.delete()?;
+                print!("deleted {name}");
+            }
+        }
+        Cmd::Tx { name, message } => {
+            let qname = Name::new(&name)?;
+            let q = Queue::open(qname).expect("open");
+            q.send(&Message {
+                data: message.into_bytes(),
+                priority: 0,
+            }).expect("send");
+        }
     }
-    println!(
-        "podman:\n    sudo podman run --rm -it --ipc=ns:/proc/{pid}/ns/ipc -v $HOME/.cargo/bin:/bin:ro ubuntu:22.04 mq_cli ls"
-    );
-
-    let name = Name::new(&opts.name)?;
-    let queue = Queue::create(name, 1, 128).expect("failed to create queue");
-    println!("queue created successfully");
-
-    let result = queue.receive()?;
-    println!("received: {:?}", String::from_utf8(result.data)?);
-
-    queue.delete()?;
 
     Ok(())
 }
 
+fn list_namespaces(pid: Pid) -> Result<String> {
+    Ok(String::from_utf8(
+        Command::new("lsns").stdout(Stdio::piped()).spawn()?.wait_with_output()?.stdout,
+    )?)
+}
+
 fn find_namespace(pid: Pid) -> Result<String> {
-    let out = String::from_utf8(Command::new("lsns").stdout(Stdio::piped()).spawn()?.wait_with_output()?.stdout)?;
-    out.lines().find(|l| l.contains(&pid.to_string())).map(|s| s.to_owned()).ok_or(anyhow!(""))
+    let ns_list = list_namespaces(pid)?;
+    ns_list
+        .lines()
+        .find(|l| l.contains(&pid.to_string()))
+        .map(|s| s.to_owned())
+        .ok_or(anyhow!("no ns for {pid}"))
+}
+
+fn list_mqs() -> Result<Vec<String>> {
+    let list: Vec<_> = String::from_utf8(Command::new("mq_cli").arg("ls").output().unwrap().stdout)?
+        .lines()
+        .map(|l| l.split_once(':'))
+        .flatten()
+        .map(|(s, _)| s.to_owned())
+        .collect();
+    Ok(list)
+}
+
+fn make_podman_cmd(pid: Pid, q_name: &str, image_name: &str) -> String {
+    format!(
+        r#"sudo podman run --rm -it --ipc=ns:/proc/{pid}/ns/ipc {image_name} bash -c 'mq_cli ls && mq_cli send {q_name} "Hello, from podman"'"#
+    )
 }
